c2300c901d21d788ce24bac7016f5fbdd5c2f8e1
andrewalson <83783605+andrewalson@users.noreply.github.com>
2024-05-29 19:34:28 -0400

    Initial Commit

---
+++ c867_roster_parser/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  c867_roster_parser
+//
+//  Created by Andrew Alson on 5/29/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
