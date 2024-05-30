7df3ce72b659ab143138b5ad23c64d97b61bafc8
andrewalson <83783605+andrewalson@users.noreply.github.com>
2024-05-29 20:50:42 -0400

    Add complete student header and implementation files

--- c867_roster_parser/student.cpp
+++ c867_roster_parser/student.cpp
@@ -1,8 +1,90 @@
-//
-//  student.cpp
-//  c867_roster_parser
-//
-//  Created by Andrew Alson on 5/29/24.
-//
-
 #include "student.hpp"
+#include "degree.hpp"
+#include <iostream>
+#include <string>
+using namespace std;
+
+// defining paramaterized student constructor
+Student::Student(string studentID, string firstName, string lastName, string emailAddress, int age, int daysPerCourse[], DegreeProgram degreeProgram) {
+    this->studentID = studentID;
+    this->firstName = firstName;
+    this->lastName = lastName;
+    this->emailAddress = emailAddress;
+    this->age = age;
+    this->degreeProgram = degreeProgram;
+    // we know there will always be three courses/days values
+    this->daysPerCourse[0] = daysPerCourse[0];
+    this->daysPerCourse[1] = daysPerCourse[1];
+    this->daysPerCourse[2] = daysPerCourse[2];
+    }
+
+// defining getters (accessors)
+string Student::getStudentID() {
+    return studentID;
+}
+
+string Student::getFirstName() {
+    return firstName;
+}
+string Student::getLastName() {
+    return lastName;
+}
+
+string Student::getEmailAddress() {
+    return emailAddress;
+}
+
+int Student::getAge() {
+    return age;
+}
+
+int Student::getDaysPerCourse(int arrayPosition) {
+    return daysPerCourse[arrayPosition];
+}
+
+DegreeProgram Student::getDegreeProgram() {
+    return degreeProgram;
+}
+
+// defining setters (mutators)
+void Student::setStudentID(string newStudentID) {
+    this->studentID = newStudentID;
+}
+
+void Student::setFirstName(string newFirstName) {
+    this->firstName = newFirstName;
+}
+
+void Student::setLastName(string newLastName) {
+    this->lastName = newLastName;
+}
+
+void Student::setEmailAddress(string newEmailAddress) {
+    this->emailAddress = newEmailAddress;
+}
+
+void Student::setAge(int newAge) {
+    this->age = newAge;
+}
+
+void Student::setDaysPerCourse(int newDaysPerCourse, int arrayPosition) {
+    this->daysPerCourse[arrayPosition] = newDaysPerCourse;
+}
+
+void Student::setAllDaysPerCourses(int newDaysPerCourse[]) {
+    daysPerCourse[0] = newDaysPerCourse[0];
+    daysPerCourse[1] = newDaysPerCourse[1];
+    daysPerCourse[2] = newDaysPerCourse[2];
+//    for (int i = 0; i < DaysInCourseArraySize; i++) {
+//        this->daysPerCourse[i] = newDaysPerCourse[i];
+//    }
+}
+
+void Student::setDegreeProgram(DegreeProgram newDegreeProgram) {
+    this->degreeProgram = newDegreeProgram;
+}
+
+// print method formatted according to requirements
+void Student::print() {
+    cout << studentID << "\t" << "First Name: " << firstName << "\t" << "Last Name: " << lastName << "\t" << "Age: " << age << "daysInCourse: {" << daysPerCourse[0] << ", " << daysPerCourse[1] << ", " << daysPerCourse[2] << "} Degree Program: " << endl;
+}
--- c867_roster_parser/student.hpp
+++ c867_roster_parser/student.hpp
@@ -1,13 +1,47 @@
-//
-//  student.hpp
-//  c867_roster_parser
-//
-//  Created by Andrew Alson on 5/29/24.
-//
+#pragma once
+#include <iostream>
+#include <string>
+#include "degree.hpp"
+using std::string;
 
-#ifndef student_hpp
-#define student_hpp
-
-#include <stdio.h>
-
-#endif /* student_hpp */
+class Student {
+public:
+    // no default constructor according to project requirements
+    // paramaterized constructor
+    Student(string studentID, string firstName, string lastName, string emailAddress, int age, int daysPerCourse[], DegreeProgram degreeProgram);
+    
+    // declaring getters (accessors)
+    string getStudentID();
+    string getFirstName();
+    string getLastName();
+    string getEmailAddress();
+    int getAge();
+    int getDaysPerCourse(int arrayPosition);
+    DegreeProgram getDegreeProgram();
+    
+    // setters (mutators)
+    void setStudentID(string newStudentID);
+    void setFirstName(string newFirstName);
+    void setLastName(string newLastName);
+    void setEmailAddress(string newEmailAddress);
+    void setAge(int newAge);
+    void setDaysPerCourse(int newDaysPerCourse, int arrayPosition);
+    void setAllDaysPerCourses(int newDaysPerCourse[]);
+    void setDegreeProgram(DegreeProgram degreeProgram);
+    
+    // print method
+    void print();
+    
+    // int constant could allow passing array of courses?
+    // const static int numberCourses = 3;
+    
+private:
+    // student variables
+    string studentID;
+    string firstName;
+    string lastName;
+    string emailAddress;
+    int age;
+    int daysPerCourse[3];
+    DegreeProgram degreeProgram;
+};
