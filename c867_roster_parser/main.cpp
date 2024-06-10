42efb2de09154ef4600322ed4a8059317037abda
andrewalson <83783605+andrewalson@users.noreply.github.com>
2024-06-10 17:57:43 -0400

    Manual testing & project files

--- c867_roster_parser/main.cpp
+++ c867_roster_parser/main.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 #include "degree.hpp"
 #include "student.hpp"
+#include "roster.hpp"
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    
+        
     const string studentData[] =
     {"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY", "A5,Andrew,Alson,aalson@wgu.edu,24,30,40,25,SOFTWARE"};
     
+    cout << "C867 Scripting and Programming Applications in C++" << endl;
+    cout << "Class Roster Performance Assessment" << endl;
+    cout << "Andrew Alson (Student ID: 011869329)" << endl;
+    
+    Roster classRoster; // Statically allocated roster instance
+    
+//    for (int i = 0; i < sizeof(classRoster.classRosterArray); i++ ) {}
+    
+    cout << "Displaying all students from course:" << endl;
+    
+    cout << "Displaying invalid emails:" << endl;
+    
+    
+    
     // TESTING //
-    // ******* //
     
     cout << studentData[1] << endl;
     
@@ -18,14 +32,21 @@ int main(int argc, const char * argv[]) {
     tester.setLastName("Olson");
     cout << "Now, last name: " << tester.getLastName() << endl;
     
-    // PROD //
-    // **** //
+    classRoster.parse("A5,Andrew,Alson,aalson@wgu.edu,24,5,20,25,SOFTWARE");
+    cout << classRoster.classRosterArray[0] << endl; // Prints memory address of array index
     
-    cout << "C867 Scripting and Programming Applications in C++" << endl;
-    cout << "Class Roster Performance Assessment" << endl;
-    cout << "Andrew Alson (Student ID: 011869329)" << endl;
+    Student* current = classRoster.classRosterArray[0];
+    cout << current->getFirstName() << endl; // Test a method
     
-    cout << "Display all students from course:" << endl;
+    Student* testerPointer = &tester;
+    cout << testerPointer->getEmailAddress() << endl;
     
+    cout << "continuity test" << endl;
+    
+    //for (int i = 0; i < sizeof(classRoster.classRosterArray); i++) {
+        //Student* current = classRoster.classRosterArray[i];
+        //cout << current->getFirstName();
+        //Student::getFirstName(classRoster.classRosterArray[i]);
+    //}
     
 }
