249e1b694f075db1861a45efe8fa7f5fdcf968f6
andrewalson <83783605+andrewalson@users.noreply.github.com>
2024-06-15 20:26:29 -0400

    String array for degree conversion

--- c867_roster_parser/degree.hpp
+++ c867_roster_parser/degree.hpp
@@ -1,8 +1,11 @@
 #pragma once
-#include <iostream>
+#include <string>
+using namespace std;
 
 enum DegreeProgram {
     SECURITY,
     NETWORK,
     SOFTWARE
 };
+
+static const string strDegreeProgram[] = {"Security", "Network", "Software"};
